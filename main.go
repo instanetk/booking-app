b5937637aa08b3c765357ca3c0b64bd37c6c4708
David Quintero <davidq@gmail.com>
2022-01-12 14:49:40 -0500

    validating user input

--- main.go
+++ main.go
@@ -36,7 +36,11 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		if userTickets <= remainingTickets {
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
 
 			bookings = append(bookings, firstName+" "+lastName)
@@ -59,8 +63,17 @@ func main() {
 				break
 			}
 		} else {
-			fmt.Printf("We only have %v tickets available, unable to book %v tickets.\n", remainingTickets, userTickets)
+			if !isValidName {
+				fmt.Println("First name or Last name entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address does not have @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets entered is not valid")
+			}
 
+			fmt.Println("Your input data is invalid, try again.")
 		}
 
 	}
